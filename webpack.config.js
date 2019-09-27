e9c7d549de32bb553e1c32213eb555237f65146a
Ujjwal Goel <ugoel13@gmail.com>
2019-09-27 20:47:06 +0530

    path value corrected

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   entry: "./src/index",
   output: {
-    filename: path.resolve(__dirname, "./src/dist"),
+    path: path.resolve(__dirname, "./src/dist"),
     filename: "bundle.js"
   }
 };
